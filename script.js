a4cacc328a71d85628bc90336a8bc6e39686653a
eisa-ahmed-08 <eisa.ahmed@iisgalvanimi.edu.it>
2026-04-15 12:51:15 +0000

    mimi

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ var myGameArea = {
         this.canvas.width = 480;
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
-        document.body.appendChild(this.canvas);
-        this.interval = setInterval(updateGameArea, 20); 
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        
+    
+        window.addEventListener('keydown', function (e) {
+            if (e.key === "ArrowUp") moveup();
+            if (e.key === "ArrowDown") movedown();
+            if (e.key === "ArrowLeft") moveleft();
+            if (e.key === "ArrowRight") moveright();
+        });
+        window.addEventListener('keyup', function (e) {
+            stopMove();
+        });
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -22,35 +33,44 @@ var myGameArea = {
 };
 
 var animatedObject = {
-  speedX: 0,
-  speedY: 0,
-  width: 100,
-  height: 120,
-  x: 10,
-  y: 120,
-  imageList: [], 
-  contaFrame: 0, 
-  actualFrame: 0, 
+    speedX: 0,
+    speedY: 0,
+    width: 70, 
+    height: 100,
+    x: 10,
+    y: 120,
+    imageList: [], 
+    contaFrame: 0, 
+    actualFrame: 0, 
 
-  update: function() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    this.contaFrame++;
-    if (this.contaFrame == 50) {
-      this.contaFrame = 0;
-      this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
-      this.image = this.imageList[this.actualFrame];
-    }
-  },
+    update: function() {
+   
+        this.x += this.speedX;
+        this.y += this.speedY;
 
-  loadImages: function() {
-     for (imgPath of running) {
-      var img = new Image(this.width, this.height);
-      img.src = imgPath;
-      this.imageList.push(img);
+   
+        if (this.x < 0) this.x = 0;
+        if (this.x > myGameArea.canvas.width - this.width) this.x = myGameArea.canvas.width - this.width;
+        if (this.y < 0) this.y = 0;
+        if (this.y > myGameArea.canvas.height - this.height) this.y = myGameArea.canvas.height - this.height;
+
+       
+        this.contaFrame++;
+        if (this.contaFrame >= 4) {
+            this.contaFrame = 0;
+            this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+            this.image = this.imageList[this.actualFrame];
+        }
+    },
+
+    loadImages: function() {
+        for (let imgPath of running) {
+            var img = new Image();
+            img.src = imgPath;
+            this.imageList.push(img);
+        }
+        this.image = this.imageList[0];
     }
-    this.image = this.imageList[this.actualFrame];
-  }
 };
 
 function startGame() {
@@ -64,18 +84,11 @@ function updateGameArea() {
     myGameArea.drawGameObject(animatedObject);
 }
 
-function moveup() {
-   animatedObject.y -= 20;
-}
-
-function movedown() {
-    animatedObject.y += 20;
-}
-
-function moveleft() {
-    animatedObject.x -= 20;
-}
-
-function moveright() {
-    animatedObject.x += 20;
+function moveup() { animatedObject.speedY = -3; }
+function movedown() { animatedObject.speedY = 3; }
+function moveleft() { animatedObject.speedX = -3; }
+function moveright() { animatedObject.speedX = 3; }
+function stopMove() { 
+    animatedObject.speedX = 0; 
+    animatedObject.speedY = 0; 
 }
--- sprite.js
+++ sprite.js
@@ -1,12 +1,12 @@
 var running = [
-"https://i.ibb.co/VpV8X5ws/Run-002.png",
-"https://i.ibb.co/j9QwQFP6/Run-003.png",
-"https://i.ibb.co/5Whn6NVq/Run-004.png",
-"https://i.ibb.co/HLNLrGY1/Run-005.png",
-"https://i.ibb.co/kVt79YmM/Run-006.png",
-"https://i.ibb.co/pv4DDGr2/Run-007.png",
-"https://i.ibb.co/C50GrbX6/Run-008.png",
-"https://i.ibb.co/hRhQfMT8/Run-009.png",
-"https://i.ibb.co/kgjWHZQz/Slide-000.png",
-"https://i.ibb.co/BHY3RkrV/Slide-001.png"
-]
+    "https://i.ibb.co/VpV8X5ws/Run-002.png",
+    "https://i.ibb.co/j9QwQFP6/Run-003.png",
+    "https://i.ibb.co/5Whn6NVq/Run-004.png",
+    "https://i.ibb.co/HLNLrGY1/Run-005.png",
+    "https://i.ibb.co/kVt79YmM/Run-006.png",
+    "https://i.ibb.co/pv4DDGr2/Run-007.png",
+    "https://i.ibb.co/C50GrbX6/Run-008.png",
+    "https://i.ibb.co/hRhQfMT8/Run-009.png",
+    "https://i.ibb.co/kgjWHZQz/Slide-000.png",
+    "https://i.ibb.co/BHY3RkrV/Slide-001.png"
+];
